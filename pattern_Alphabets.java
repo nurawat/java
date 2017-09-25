8a968e28240944c1b3814742ddb7ed829d129823
=Arun Rawat <rawatarun65@gmail.com>
2017-09-25 17:00:48 +0530

    Added all alphabets

--- pattern_Alphabets.java
+++ pattern_Alphabets.java
@@ -1,23 +1,23 @@
 //
-// This programs prints Alphabets using Star(*) First it takes an Input
+// This programs prints Alphabets using Star(* ) First it takes an Input
 // From the User Like
 //
 // @example:
 // Enter the alphabet You need Pattern of : AaAa
 // Printing A:
-// *****
-// *   *
-// *****
-// *   *
-// *   *
+// * * * * * 
+// *    * 
+// * * * * * 
+// *    * 
+// *    * 
 //
 // Done !!!
 // Want to print more (y/n):n
 // System Exiting !!!
-// @ Classes used: java.io.*
+// @ Classes used: java.io.* 
 //                 java.util.Scanner
 
-import java.io.*;
+import java.io.* ;
 import java.util.Scanner;
 
 class pattern_Alphabets
@@ -37,6 +37,7 @@ class pattern_Alphabets
 
       for(string_loop=0;string_loop<pattern.length();string_loop++)
       {
+        System.out.println("\nPrinting Character "+ pattern.charAt(string_loop)+" : ");
         switch(pattern.toUpperCase().charAt(string_loop))
         {
           case 'A':
@@ -47,10 +48,10 @@ class pattern_Alphabets
               {
                 if(i==0 || j==4 || j==0 || i==(height/2))
                 {
-                  System.out.print("*");
+                  System.out.print("* ");
                 }
                 else
-                  System.out.print(" ");
+                  System.out.print("  ");
               }
             }
         break;
@@ -62,19 +63,406 @@ class pattern_Alphabets
               {
                 if(i==0 || i==4 || j==4 || j==0 || i==(height/2))
                 {
-                  System.out.print("*");
+                  System.out.print("* ");
                 }
                 else
-                  System.out.print(" ");
+                  System.out.print("  ");
               }
             }
         break;
-          default: System.out.println("Something Went Wrong");
+
+        case 'C':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(i==0 || i==4 || j==0)
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'D':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(i==0 || i==4 || j==4 || j==1)
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'E':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(i==0 || i==4 || j==0 || i==(height/2))
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'F':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(i==0 || j==0 || i==(height/2))
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'G':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(i==0 || j==0 || i==(height/2) || i==4 || (j==4 && i>height/2))
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'H':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(j==4 || j==0 || i==(height/2))
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'I':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(i==0 || i==4 || j==(height/2))
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'J':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(i==0 || (i==4 && j<height/2) || j==(height/2) )
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'K':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(j==height/2 || (i==(height-1-j) && i<=height/2) || (i==j && j>=height/2) )
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'L':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(j==0 || i==4)
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'M':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(j==0 || j==4 || i== 0 || j == height/2)
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'N':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(j==0 || j==4 || (i==j))
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'O':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(j==0 || j==4 || i==0 || i==4)
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'P':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(j==0 || i==height/2 || i==0 || (j==4 && i<height/2))
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'Q':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(i==0 || j==0 || i==4 || j==4 || (i==j && i>height/2))
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+            System.out.print(" * ");
+        break;
+
+        case 'R':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(i==0 || j==0 || (j==4 && i<height/2) || (i==j && i>=height/2))
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'S':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(i==0 || i==4 || (j==0 && i<height/2) || i==height/2 || (j==4 && i> height/2))
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'T':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(i==0 || j==height/2)
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'U':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(j==0 || j==4 || i==4)
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'V':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if((j==1 && i!=4) || (j==3 && i!=4) || (i==4 && j==height/2))
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'W':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(j==0 || j==4 || j==height/2 || i==4)
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+
+        case 'X':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(i==j || (i==((height-1)-j)))
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'Y':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if( (i==j && i<=height/2) || (i==((height-1)-j) && i<=height/2) || (j==height/2 && i>=height/2))
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        case 'Z':
+            for(i=0;i<5;i++)
+            {
+              System.out.println();
+              for(j=0;j<5;j++)
+              {
+                if(i==0 || i==4 || (i==(height-1)-j))
+                {
+                  System.out.print("* ");
+                }
+                else
+                  System.out.print("  ");
+              }
+            }
+        break;
+
+        default: System.out.println("Something Went Wrong");
         }
       }
-      // System.out.print("Want to Do it Again");
-      // choice = s.next().charAt(0);
+      System.out.println("Want to Do it Again (y/n) :");
+      choice = s.next().charAt(0);
     }while(choice=='y' || choice=='Y');
-    System.out.print("System Exiting !!!");
+    // System.out.print("System Exiting !!!");
   }
 }
